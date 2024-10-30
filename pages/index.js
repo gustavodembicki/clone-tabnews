3317b71975e1e2c790db0ca021db1a52e74b5e1e
Gustavo Dembicki <gustavodembicki@hotmail.com>
2024-10-30 20:45:16 -0300

    Initial pages

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Eu te amo, nunca desista dos sonhos e lembre-se: Os desafios sao melhores, quando os encaramos com sorriso, e não com raiva ou odio! s2</h1>
+}
+
+export default Home;
